4ee4d649c7fee22da653a04b0a7bcc7c0135f0b3
Graham Otte <go@grahamotte.com>
2020-05-31 12:52:25 -0700

    Updates

---
+++ precache-manifest.01bc125a9a1b2125752bed6ef87a13cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67b0554911cac1762efcdde527f58645",
+    "url": "/lettersoup/index.html"
+  },
+  {
+    "revision": "6593b3f9bc8d55b96d85",
+    "url": "/lettersoup/static/css/main.dd488603.chunk.css"
+  },
+  {
+    "revision": "9c12e114ad8b80a0d0fe",
+    "url": "/lettersoup/static/js/2.06b0614d.chunk.js"
+  },
+  {
+    "revision": "806a98f318263a7af6eb94d73465b791",
+    "url": "/lettersoup/static/js/2.06b0614d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6593b3f9bc8d55b96d85",
+    "url": "/lettersoup/static/js/main.48065f01.chunk.js"
+  },
+  {
+    "revision": "0ff454d8228f2c87f766",
+    "url": "/lettersoup/static/js/runtime-main.c54ea8a1.js"
+  }
+]);
